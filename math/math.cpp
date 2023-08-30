883e3d54aaea38a2a558def4662668663121236f
zhengye shi <mansteinshi@gmail.com>
2023-08-30 23:59:15 +0800

    添加项目文件。

---
+++ math/math.cpp
@@ -0,0 +1,27 @@
+﻿// math.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+	cout << "Hello my name is Zhengye Shi and I am going to solve the equation:\nAx + B = 0\nFor x\n\nPlease enter a value for A: ";
+	cin >> A;
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	cout << "\nSolving " << A << "x+" << B << "=0 for x...\n" << "\nThe answer is "<<(0-B)/A<<endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
